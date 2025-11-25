cf479c595b985c1d3ddf7f444b0f1e6dfb395a75
Stanyslas Bres <dzetah@pm.me>
2025-11-25 23:54:09 +0100

    chore: add eslint-plugin-import-x and order rule

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,10 @@
-import globals from 'globals';
-import { globalIgnores } from 'eslint/config';
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import stylistic from '@stylistic/eslint-plugin';
-import pluginVue from 'eslint-plugin-vue';
 import pluginVitest from '@vitest/eslint-plugin';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import { globalIgnores } from 'eslint/config';
+import { rules as importXRules } from 'eslint-plugin-import-x';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -52,6 +53,26 @@ export default defineConfigWithVueTs(
     },
   },
   vueTsConfigs.recommended,
+
+  {
+    plugins: {
+      import: {
+        rules: {
+          order: importXRules.order,
+        },
+      },
+    },
+    rules: {
+      'import/order': ['error', {
+        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+        'newlines-between': 'always',
+        'alphabetize': {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      }],
+    },
+  },
 
   {
     ...pluginVitest.configs.recommended,
--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,6 +1,6 @@
+import { mount } from '@vue/test-utils';
 import { describe, it, expect } from 'vitest';
 
-import { mount } from '@vue/test-utils';
 import App from '../App.vue';
 
 describe('App', () => {
